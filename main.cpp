e982302db6667c0b2362fbaed5f52207a0ee3448
Brayan <2995454660101@ingenieria.usac.edu.gt>
2022-02-08 17:30:30 -0600

    Inicial

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+struct Profesor
+{
+    int id_profesor;
+    char cui[13];
+    char nombre[25];
+    char curso[25];
+};
+
+struct Estudiante
+{
+    int id_estudiante;
+    char cui[13];
+    char nombre[25];
+    char carnet[10];
+};
+
+int main() {
+    cout<<"Bienvenido"<<endl;
+    return 0;
+}
